2e1ae71076870d1acdae85f59de051ae9553a87e
jcostadev23 <carlos_jesus_costa@hotmail.com>
2025-03-11 22:47:22 +0000

    correction

--- frontend/src/app/components/Button/index.tsx
+++ frontend/src/app/components/Button/index.tsx
@@ -1,11 +1,30 @@
 import { ClickEvent } from "../types";
 
-type Props = {
-  type?: "button" | "submit" | "reset";
-  onClick?: ClickEvent;
-  children: React.ReactNode;
+const buttonColors = {
+  primary: "bg-blue-500",
+  secondary: "bg-gray-500",
+  danger: "bg-red-500",
 };
-const Button: React.FC<Props> = ({ type = "button", onClick, children }) => {
+
+type Props = {
+  type: "button" | "submit" | "reset";
+  onClick?: ClickEvent;
+  label: string;
+  backgroundColor: "primary" | "secondary" | "danger";
+  applyMinWidth: boolean;
+  size?: string;
+};
+const Button: React.FC<Props> = ({
+  type = "button",
+  onClick,
+  label = "Click",
+  applyMinWidth = false,
+  backgroundColor = "primary",
+  size = "medium",
+  ...props
+}) => {
+  const width = applyMinWidth ? "min-w-20 px-2 py-1" : "min-w-50 px-4 py-2";
+
   const handleClick: ClickEvent = (event) => {
     if (onClick) {
       onClick(event);
@@ -14,11 +33,12 @@ const Button: React.FC<Props> = ({ type = "button", onClick, children }) => {
 
   return (
     <button
-      className="bg-gray-500 text-white font-bold px-4 py-2 rounded-3xl"
+      className={`${buttonColors[backgroundColor]} text-white font-bold rounded-3xl ${width}`}
       type={type}
       onClick={handleClick}
+      {...props}
     >
-      {children}
+      {label}
     </button>
   );
 };
