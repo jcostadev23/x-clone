c3a86ee46cd70d466298c56f2dde5354c03768f1
jcostadev23 <carlos_jesus_costa@hotmail.com>
2025-04-22 21:43:13 +0100

    adding-logic-to-reducer

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,27 +1,24 @@
-import PostTweet from "./components/PostTweet";
 import Menu from "./components/Menu";
+import PostTweet from "./components/PostTweet";
 import ShowMorePosts from "./components/ShowMorePosts";
 import Sugestions from "./components/Sugestions/RhsSugestions";
-import TweetCard from "./components/Tweets/TweetCard";
+import Tweets from "./components/Tweets";
 import { AppProvider } from "./hooks/useAppContext";
-import { Tweet } from "./types";
-import { getAllTweets } from "./utils/apiCalls";
+import { ReducerProvider } from "./hooks/useReducer";
 
 export default async function Home() {
-  const tweets: Array<Tweet> = await getAllTweets();
-
   return (
     <div className="flex flex-col gap-6 p-5 md:flex-row md:gap-8 bg-gray-50 dark:bg-gray-800">
       <AppProvider>
-        <Menu />
-        <div className="flex-[4] container mx-auto border border-gray-300">
-          <PostTweet />
-          <ShowMorePosts />
-          {tweets.map((tweet) => (
-            <TweetCard key={tweet._id} tweet={tweet} />
-          ))}
-        </div>
-        <Sugestions />
+        <ReducerProvider>
+          <Menu />
+          <div className="flex-[4] container mx-auto border border-gray-300">
+            <PostTweet />
+            <ShowMorePosts />
+            <Tweets />
+          </div>
+          <Sugestions />
+        </ReducerProvider>
       </AppProvider>
     </div>
   );
