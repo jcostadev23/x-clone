86c8cd521b13a022fc2df0079146d81794998ae2
jcostadev23 <carlos_jesus_costa@hotmail.com>
2025-04-26 13:00:37 +0100

    create user form

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,23 +3,20 @@ import PostTweet from "./components/PostTweet";
 import ShowMorePosts from "./components/ShowMorePosts";
 import Sugestions from "./components/Sugestions/RhsSugestions";
 import Tweets from "./components/Tweets";
-import { AppProvider } from "./hooks/useAppContext";
 import { ReducerProvider } from "./hooks/useReducer";
 
 export default async function Home() {
   return (
     <div className="flex flex-col gap-6 p-5 md:flex-row md:gap-8 bg-gray-50 dark:bg-gray-800">
-      <AppProvider>
-        <ReducerProvider>
-          <Menu />
-          <div className="flex-[4] container mx-auto border border-gray-300">
-            <PostTweet />
-            <ShowMorePosts />
-            <Tweets />
-          </div>
-          <Sugestions />
-        </ReducerProvider>
-      </AppProvider>
+      <ReducerProvider>
+        <Menu />
+        <div className="flex-[4] container mx-auto border border-gray-300">
+          <PostTweet />
+          <ShowMorePosts />
+          <Tweets />
+        </div>
+        <Sugestions />
+      </ReducerProvider>
     </div>
   );
 }
--- frontend/src/app/user/page.tsx
+++ frontend/src/app/user/page.tsx
@@ -1,34 +1,42 @@
 "use client";
 
+import clsx from "clsx";
 import Image from "next/image";
 import { useState } from "react";
 import xIcon from "../../public/x-logo.png";
 import Button from "../components/Button";
+import Loading from "../components/Loader";
 import Modal from "../components/Modal";
 import UserForm from "../components/User/Form";
+import { useAppContext } from "../hooks/useAppContext";
 
 const UserPage = () => {
+  const { isLoading } = useAppContext();
   const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="flex items-center justify-evenly h-full w-full">
-      <Image width={300} height={300} alt="x image" src={xIcon} />
-      <div className="flex items-center">
-        <div className="flex flex-col gap-2">
-          <Button
-            onClick={() => setIsOpen(true)}
-            label="Create account"
-          ></Button>
-          <Button
-            onClick={() => setIsOpen(true)}
-            label="Sign in"
-            backgroundColor="secondary"
-          ></Button>
+    <>
+      {isLoading && <Loading />}
+      <div
+        className={clsx(
+          "flex items-center justify-evenly h-full w-full",
+          isOpen && "opacity-70 bg-gray-50"
+        )}
+      >
+        <Image width={300} height={300} alt="x image" src={xIcon} />
+        <div className="flex items-center">
+          <div className="flex flex-col gap-2">
+            <Button
+              onClick={() => setIsOpen(true)}
+              label="Create account"
+            ></Button>
+          </div>
         </div>
-        <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
-          <UserForm></UserForm>
-        </Modal>
       </div>
-    </div>
+      <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+        <UserForm handleClose={setIsOpen} />
+      </Modal>
+    </>
   );
 };
 
