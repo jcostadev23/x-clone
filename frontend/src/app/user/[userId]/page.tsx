2f23f5f4256b370ed70e1c6578f938b1334cb032
jcostadev23 <carlos_jesus_costa@hotmail.com>
2025-07-08 21:46:50 +0100

    Adding the FollowUsers to user page

--- frontend/src/app/user/[userId]/page.tsx
+++ frontend/src/app/user/[userId]/page.tsx
@@ -1,7 +1,8 @@
+import FollowUsers from "@/components/FollowUsers";
 import { getUserById } from "@/utils/apiCalls";
 
 type Props = {
-  params: { userId: number };
+  params: { userId: string };
 };
 
 const UserPage: React.FC<Props> = async ({ params }) => {
@@ -9,8 +10,9 @@ const UserPage: React.FC<Props> = async ({ params }) => {
   const user = await getUserById(userId);
 
   return (
-    <div className="flex justify-center align-middle text-3xl">
+    <div className="flex gap-2 justify-center align-middle text-3xl">
       {user.userName}
+      <FollowUsers />
     </div>
   );
 };
