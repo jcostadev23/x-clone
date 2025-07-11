2fe77481423087f769cac5c4f50e3d833b561fd5
jcostadev23 <carlos_jesus_costa@hotmail.com>
2025-07-11 21:09:33 +0100

    Getting tweets from server side

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,12 +4,14 @@ import ShowMorePosts from "@/components/ShowMorePosts";
 import Sugestions from "@/components/Sugestions/RhsSugestions";
 import Tweets from "@/components/Tweets";
 import { ReducerProvider } from "@/hooks/useReducer";
+import { getAllTweets } from "@/utils/apiCalls";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
   const cookieStore = await cookies();
-  const token = cookieStore.get("token");
+  const token = cookieStore.get("token")?.value;
+  const tweets = await getAllTweets({ token });
 
   if (!token) {
     redirect("/signin");
@@ -17,12 +19,12 @@ export default async function Home() {
 
   return (
     <div className="flex flex-col gap-6 p-5 md:flex-row md:gap-8">
-      <ReducerProvider>
+      <ReducerProvider tweets={tweets}>
         <Menu />
         <div className="flex-[4] container mx-auto border border-gray-300">
           <PostTweet />
           <ShowMorePosts />
-          <Tweets />
+          <Tweets tweets={tweets} />
         </div>
         <Sugestions />
       </ReducerProvider>
--- frontend/src/components/Tweets/index.tsx
+++ frontend/src/components/Tweets/index.tsx
@@ -1,42 +1,14 @@
-"use client";
-
-import { ActionsType } from "@/helpers/reducer";
-import { useAppContext } from "@/hooks/useAppContext";
-import { useReducerContext } from "@/hooks/useReducer";
-import { getAllTweets } from "@/utils/apiCalls";
-import { formatDate } from "@/utils/dateFormater";
-import { useEffect } from "react";
-import Loading from "../Loader";
+import { Tweet } from "@/types";
 import TweetCard from "./TweetCard";
 
-const Tweets = () => {
-  const { isLoading, setIsLoading } = useAppContext();
-  const { state, dispatch } = useReducerContext();
+type Props = {
+  tweets: Array<Tweet>;
+};
 
-  useEffect(() => {
-    (async () => {
-      setIsLoading(true);
-      const tweets = await getAllTweets();
-      setIsLoading(false);
-
-      if (!tweets) {
-        return false;
-      }
-
-      dispatch({
-        type: ActionsType.SET_TWEETS,
-        payload: tweets.map((tweet) => ({
-          ...tweet,
-          date: formatDate(tweet.date),
-        })),
-      });
-    })();
-  }, [setIsLoading, dispatch]);
-
+const Tweets = async ({ tweets }: Props) => {
   return (
     <>
-      {isLoading && <Loading />}
-      {state.tweets.map((tweet) => (
+      {tweets?.map((tweet) => (
         <TweetCard key={tweet._id} tweet={tweet} />
       ))}
     </>
